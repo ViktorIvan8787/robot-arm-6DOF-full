834af2af4c0e1ddad062fb46e9e83d99fb78f372
Tauqir, Umar U <muhammadumartauqir@gmail.com>
2026-08-23 14:21:53 +0100

    Refactor code in detect.py
    Remove unneccessary comments
    
    Remove old code

--- python/camera/detect.py
+++ python/camera/detect.py
@@ -62,36 +62,3 @@ def detect(detections: list[Detection], target_class: str, min_confidence: float
     # Otherwise pick the best match (detection with highest confidence)
     best_match = max(candidates, key = lambda d: d.confidence)
     return DetectionResult(True, best_match)
-# Open camera make sure that it reads frame properly and capture the frame
-# Capture a still image from the video camera {default camera: 0} 
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
-cap.release()
-
-# Check if the frame was read correctly using the ret variable
-if not ret :
-    raise RuntimeError("Failed to capture image")
-
-# Otherwise we can pass the frame into the model and start detecting
-cv2.imwrite("captured_frame.jpg", frame)
-
-# Get the results of passing the captured frame into the model 
-results = model("captured_frame.jpg")
-
-# Iterate over the results and find the object that you are looking for 
-
-target_class = "cup"
-
-for image in results:
-    for object in image.boxes:
-        # get the class name of the object to compare to the target class
-        class_name = model.name[int(object.cls[0])]
-        # get the confidence for this object 
-        confidence = float(object.conf[0])
-
-        # check if it is the right object
-        if target_class == class_name:
-            # Get centre coordinates of the object
-            x , y , _ , _ = object.xywh[0]
-            # Print out that object has been found for test units and debugging
-            print(f"{target_class} object found, (conf {confidence:.2f}) at pixel coordinates ({x:.0f}, {y:.0f})")
